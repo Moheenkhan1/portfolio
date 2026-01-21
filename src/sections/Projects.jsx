e1b06668c3bd8b0249612fee4569085e6a88a598
Your Name <moheenkhanpersonal@gmail.com>
2026-01-21 23:32:09 +0530

    Project added

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -27,6 +27,22 @@ const projects = [
   },
   {
   id: "p2",
+  title: "Food Delivery Webiste - (SpringBoot - React.Js)",
+  date: "January 2026",
+  short:
+    "Online food delivery platform offering restaurant browsing, menu selection, and seamless ordering with secure checkout.",
+  long:
+    "Food Delivery Website — a modern and user-friendly platform that allows users to browse restaurants, explore menus, and place food orders online. The application focuses on responsive design, clean UI, and smooth navigation to deliver a seamless ordering experience. Built to support real-world workflows such as cart management, authentication, and checkout while maintaining performance and usability across devices",
+  tech: [ "Spring Boot", "React.js", "REST APIs", "Spring Data JPA","Tailwind CSS", "Java"],
+  media: "https://res.cloudinary.com/diydvemrv/video/upload/v1769018379/Untitled_video_-_Made_with_Clipchamp_1_ro9p8t.mp4",
+  imageURL:"https://res.cloudinary.com/diydvemrv/image/upload/v1769018423/Screenshot_2026-01-20_185801_kq7l4o.png",
+  poster: previewImagePath,
+  isVideo: true,
+  // live: "",
+  repo: "https://github.com/Moheenkhan1/fooodOrderingWebsite.git"
+},
+  {
+  id: "p3",
   title: "Personal Portfolio Website",
   date: "Mar 2025",
   short:
@@ -40,22 +56,6 @@ const projects = [
   isVideo: true,
   live: "https://moheenkhan-portfolio.vercel.app/",
   repo: "https://github.com/Moheenkhan1/portfolio.git"
-},
-  {
-  id: "p3",
-  title: "Food Delivery Webiste - (Java Full-Stack)",
-  date: "January 2026",
-  short:
-    "Online food delivery platform offering restaurant browsing, menu selection, and seamless ordering with secure checkout.",
-  long:
-    "Food Delivery Website — a modern and user-friendly platform that allows users to browse restaurants, explore menus, and place food orders online. The application focuses on responsive design, clean UI, and smooth navigation to deliver a seamless ordering experience. Built to support real-world workflows such as cart management, authentication, and checkout while maintaining performance and usability across devices",
-  tech: ["Java", "JEE", "JSP", "JDBC", "CSS", "HTML","Dao Design Pattern"],
-  media: "https://res.cloudinary.com/diydvemrv/video/upload/v1767805323/Untitled_video_-_Made_with_Clipchamp_tkcprl.mp4",
-  imageURL:"https://res.cloudinary.com/diydvemrv/image/upload/v1767805447/Screenshot_2026-01-07_222611_kox5li.png",
-  poster: previewImagePath,
-  isVideo: true,
-  // live: "",
-  repo: "https://github.com/Moheenkhan1/food_delivery.git"
 }
 ];
 
